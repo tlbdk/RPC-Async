6c2e7e252a8e5adbc8f629f9f3f4650a6bb408ed
jbj <jbj@c2514f42-cbde-0310-854e-8ab5f9d4de2d>
2007-01-13 15:24:45 +0000

    Pass working copy as an argument to svnd. This required changing listen.pl to have new cmd line convention
    git-svn-id: https://svn.rapanden.dk/svn/netlookup/trunk@1561 c2514f42-cbde-0310-854e-8ab5f9d4de2d

--- listen.pl
+++ listen.pl
@@ -1,12 +1,21 @@
 #!/usr/bin/perl -wI lib
 use strict;
+
 use RPC::Async::Server;
 use IO::URL;
 
-my $module = shift;
-@ARGV or die "Usage: $0 MODULE_FILE URL1 [ URL2 ... ]";
+@ARGV or die "Usage: $0 -u URL MODULE_FILE [ ARGS ]\n";
 
-my @fhs = map { url_listen($_) } @ARGV;
+my @urls;
+while ($ARGV[0] =~ /^-u(.*)/) {
+    shift;
+    push @urls, ($1 || shift);
+}
+shift if $ARGV[0] eq "--";
+
+my $module = shift or die;
+
+my @fhs = map { url_listen($_) } @urls;
 
 sub init_clients {
     my ($rpc) = @_;
@@ -15,7 +24,7 @@ sub init_clients {
     }
 }
 
-$0="$module";
+$0 = $module;
 
 do $module or die "Cannot load $module: $@\n";
 
